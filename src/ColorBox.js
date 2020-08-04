742348f2adea7321c325bbc926a87de90af12bd2
Yun Chung <yun.chung@mac.com>
2020-08-04 13:58:47 +0900

    Added copy overlay animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -26,7 +26,14 @@ class ColorBox extends Component {
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
+        
         <div style={{ background }} className={`copy-overlay ${copied && "show"}`} />
+        
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>copied!</h1>
+          <p>{this.props.background}</p>
+        </div>
+
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
