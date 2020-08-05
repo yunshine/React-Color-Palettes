0e68cbc60b07051b0dabab928427653e4ff24f44
Yun Chung <yun.chung@mac.com>
2020-08-05 13:49:03 +0900

    Fixed dynamic palette routes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import { Route, Switch, NavLink, Link } from 'react-router-dom';
 // import './App.css';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+  
   render() {
     return (
       <Switch> 
         <Route exact path="/" render={() => <h1>Palette List test...</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>indivudual palette test...</h1>} />
+        <Route 
+          exact 
+          path="/palette/:id" 
+          render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} 
+        />
       </Switch>
 
       // <div className="App">
