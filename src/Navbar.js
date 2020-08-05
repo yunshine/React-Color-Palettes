1702d33b712bd0917d94e2bbb6718d13153b9790
Yun Chung <yun.chung@mac.com>
2020-08-05 11:14:05 +0900

    Added a selector to the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Slider from 'rc-slider';
+import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+    // this.changeFormat = this.changeFormat.bind(this);
+  }
+
+handleChange(e) {
+  this.setState({format: e.target.value});
+  this.props.handleChange(e.target.value);
+}
+
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, handleChange } = this.props;
+    const { format } = this.state;
     return (
       <nav className="Navbar">
         <div className="logo">
@@ -19,7 +33,11 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">Hex - #57c5f7</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(228,118,214)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgb(35,168,145, 1.0)</MenuItem>
+          </Select>
         </div>
       </nav>
     );
--- src/Palette.js
+++ src/Palette.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'; // imrc is the shortcut...
+import React, { Component } from 'react';
 import Navbar from './Navbar';
 // import -something-, { -something- } from './-something-';
 import 'rc-slider/assets/index.css';
 import './Palette.css';
-// import { Route, Switch, NavLink } from 'react-router-dom';
+
 import ColorBox from './ColorBox';
 
 class Palette extends Component {
-  // static defaultProps = {
-  //   key: value,
-  // };
   
   constructor(props) {
     super(props);
     this.state = { level: 500 };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
+  }
+
+  changeFormat(val) {
+    alert(val);
   }
 
   render() {
@@ -30,8 +32,7 @@ class Palette extends Component {
 
     return (
       <div className="Palette">
-        {/* Navbar will go here... */}
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
         <div className="Palette-colors">
           {colorBoxes}
         </div>
@@ -41,4 +42,4 @@ class Palette extends Component {
   }
 }
 
-export default Palette; /// connect this component to App.js...
+export default Palette;
