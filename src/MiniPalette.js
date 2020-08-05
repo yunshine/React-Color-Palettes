4ba9e377952007a14e717d5e59ff87bc6b8c7c67
Yun Chung <yun.chung@mac.com>
2020-08-05 17:01:29 +0900

    Started styling the minipalette without the colorboxes

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -4,16 +4,33 @@ import { withTheme } from '@material-ui/styles';
 
 const styles = {
   root: {
-
+    backgroundColor: "white",
+    border: "1px solid turquoise",
+    borderRadius: "4px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: 'hidden',
+    "&:hover": {
+      cursor: "pointer"
+    }
   },
   colors: {
+    backgroundColor: "grey",
 
   }, 
   title: {
-
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative",
   }, 
   emoji: {
-
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
   }
 }
 
@@ -21,7 +38,7 @@ function MiniPalette(props) {
   const { classes, paletteName, emoji } = props;
   
   return (
-    <div className={`MiniPalette ${classes.root}`}>
+    <div className={classes.root}>
       <div className={classes.colors}></div>
   <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
     </div>
