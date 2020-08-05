bdd5d5d33da1b25628282c7579903704d660dbe8
Yun Chung <yun.chung@mac.com>
2020-08-05 13:10:11 +0900

    Added non-functioning, dynamic routes in the App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
+// import -something-, { -something- } from './-something-';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
-// import -something-, { -something- } from './-something-';
-// import { Route, Switch, NavLink } from 'react-router-dom';
+import { Route, Switch, NavLink, Link } from 'react-router-dom';
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[1])}/>
+      <Switch> 
+        <Route exact path="/" render={() => <h1>Palette List test...</h1>} />
+        <Route exact path="/palette/:id" render={() => <h1>indivudual palette test...</h1>} />
+      </Switch>
 
-        {/* <NavLink exact activeClassName="active-link" to="/-something-">-some-text-here-</NavLink> */}
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[1])}/>
+      // </div>
 
-        {/* <Switch> */}
-          {/* use path="/" to assign a default page... */}
-          {/* <Route exact path="/-something-" component={-something-} /> */}
-          {/* <Route exact path="/-something-" component={() => <-something- name='Muffins"> } /> */}
-          {/* <Route exact path="/-something-" render={() => <-something- name='Biscuit"> } /> */}
-        {/* </Switch> */}
-      </div>
     );
   }
 }
