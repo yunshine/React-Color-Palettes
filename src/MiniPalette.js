ced6de8c8a570d86572dc49cb4861ae3e9478c5f
Yun Chung <yun.chung@mac.com>
2020-08-05 15:54:36 +0900

    Installed material-ui styles and created the file for the minipalette component

---
+++ src/MiniPalette.js
@@ -0,0 +1,11 @@
+import React from 'react'; // imrc is the shortcut...
+
+function MiniPalette(props) {
+  return (
+    <div className="MiniPalette">
+      <h1>MiniPalette</h1>
+    </div>
+  )
+}
+
+export default MiniPalette;
