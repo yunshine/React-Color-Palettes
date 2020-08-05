c24ac2ea9f5685a1ae2467935e6d1c85593339fb
Yun Chung <yun.chung@mac.com>
2020-08-05 17:31:07 +0900

    Built unstyled layout for the palettelist page

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,8 +3,37 @@ import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 // import -something-, { -something- } from './-something-';
 import './PaletteList.css'; // make a CSS file for this component..
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    backgroundColor: "grey",
+  },
+};
 
 class PaletteList extends Component {
+
+  render() {
+    const { palettes, classes } = this.props;
+    return (
+      <div className={classes.root}>
+
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Color Palettes</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map(palette => (
+                <MiniPalette {...palette} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles) (PaletteList);
   // static defaultProps = {
   //   key: value,
   // };
@@ -14,19 +43,3 @@ class PaletteList extends Component {
     // this.state = { key: value };
     // this.handleClick = this.handleClick.bind(this);
   // }
-
-  render() {
-    const { palettes } = this.props;
-    return (
-      <div className="PaletteList">
-        <MiniPalette />
-        <h1>React Color Palettes</h1>
-        {palettes.map(palette => (
-            <MiniPalette {...palette} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default PaletteList;
