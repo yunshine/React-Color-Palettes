fb533019528863795c25662e6f657bdbc7e8042d
Yun Chung <yun.chung@mac.com>
2020-08-05 12:07:15 +0900

    Simple snackbar settings

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,13 +4,15 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+// import { IconButton } from '@material-ui/core';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: "hex" };
+    this.state = { format: "hex", open: true };
     this.handleChange = this.handleChange.bind(this);
     // this.changeFormat = this.changeFormat.bind(this);
   }
@@ -41,6 +43,17 @@ handleChange(e) {
             <MenuItem value="rgba">RGBA - rgb(35,168,145, 1.0)</MenuItem>
           </Select>
         </div>
+    <Snackbar 
+      anchorOrigin={{vertical: "bottom", horizontal: "left"}} 
+      open={this.state.open} 
+      autoHideDuration={2500} 
+      message={<span id='message-id'>Format changed to {this.state.format}.</span>} 
+      ContentProps={{"aria-describedby": "message-id"}} 
+      action={[
+        <IconButton>
+          <CloseIcon />
+        </IconButton>
+      ]} />
       </nav>
     );
   }
