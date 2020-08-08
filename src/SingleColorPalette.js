6ef280993967250231d464f70ff5491e8dcafac6
Yun Chung <yun.chung@mac.com>
2020-08-08 12:11:37 +0900

    Added the navbar to the singlecolorcomponent page with a functional hex/rgb/rgba selector

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'; // imrc is the shortcut...
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 // import -something-, { -something- } from './-something-';
 // import './SingleColorPalette.css'; // make a CSS file for this component..
 // import { Route, Switch, NavLink } from 'react-router-dom';
@@ -12,9 +13,15 @@ class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-    // this.state = { shades: this.gatherShades(this.props.palette) };
+    this.state = { format: "hex" };
     console.log(this._shades);
     this.gatherShades = this.gatherShades.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+
+  changeFormat(val) {
+    // alert(val);
+    this.setState({ format: val });
   }
 
   gatherShades(palette, colorToFilterBy) {
@@ -30,11 +37,13 @@ class SingleColorPalette extends Component {
   }
 
   render() {
+    const { format } = this.state;
     const colorBoxes = this._shades.map(color => (
-      <ColorBox key={color.id} name={color.name} background={color.hex} showLink={false} />
+      <ColorBox key={color.id} name={color.name} background={color[format]} showLink={false} />
     ));
     return (
       <div className="SingleColorPalette Palette">
+        <Navbar handleChange={this.changeFormat} />
         <div className="Palette-colors">
           {colorBoxes}
         </div>
