b17666ff1f5e8a51419fa0f3b3b847cc9278d528
Yun Chung <yun.chung@mac.com>
2020-08-19 12:38:57 +0900

    Added open/close functionality to the delete palette dialog box

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -4,9 +4,6 @@ import MiniPalette from './MiniPalette';
 import Dialog from '@material-ui/core/Dialog';
 import blue from '@material-ui/core/colors/blue';
 import red from '@material-ui/core/colors/red';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -15,18 +12,38 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Avatar from '@material-ui/core/Avatar';
 import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
-// import './PaletteList.css';
 import { withStyles } from '@material-ui/core/styles';
 import styles from "./styles/PaletteListStyles";
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import './PaletteList.css';
 
 class PaletteList extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      openDeleteDialog: false,
+    };
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+
+  }
+
+  openDialog() {
+    this.setState({ openDeleteDialog: true });
+  }
+  
+  closeDialog() {
+    this.setState({ openDeleteDialog: false });
+  }
+  
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`)
   }
 
   render() {
     const { palettes, classes } = this.props;
+    const { openDeleteDialog } = this.state;
+
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -38,16 +55,23 @@ class PaletteList extends Component {
             <TransitionGroup className={classes.palettes}>
               {palettes.map(palette => (
                 <CSSTransition key={palette.id} classNames='fade' timeout={500}>
-                  <MiniPalette {...palette} key={palette.id} id={palette.id} handleDelete={this.props.deletePalette} handleClick={() => this.goToPalette(palette.id)} />
+                  <MiniPalette 
+                  {...palette} 
+                  key={palette.id} 
+                  id={palette.id} 
+                  // handleDelete={this.props.deletePalette} 
+                  handleDelete={this.openDialog} 
+                  handleClick={() => this.goToPalette(palette.id)} 
+                  />
                 </CSSTransition>
               ))}
             </TransitionGroup>
           {/* </div> */}
         </div>
-        <Dialog open={true}>
-          <DialogTitle>Delete this palette?</DialogTitle>
+        <Dialog open={openDeleteDialog} aria-labelledby="delete-dialog-title" onClose={this.closeDialog}>
+          <DialogTitle id="delete-dialog-title">Delete This Palette?</DialogTitle>
           <List>
-            <ListItem>
+            <ListItem button>
               <ListItemAvatar>
                 <Avatar style={{ backgroundColor: blue[100], color: blue[600] }}>
                   <CheckIcon></CheckIcon>
@@ -55,13 +79,13 @@ class PaletteList extends Component {
               </ListItemAvatar>
               <ListItemText primary="Delete" />
             </ListItem>
-            <ListItem>
+            <ListItem button>
               <ListItemAvatar>
               <Avatar style={{ backgroundColor: red[100], color: red[600] }}>
                   <CloseIcon></CloseIcon>
                   </Avatar>
               </ListItemAvatar>
-              <ListItemText primary="Cancel" />
+              <ListItemText primary="Cancel" onClick={this.closeDialog} />
             </ListItem>
           </List>
         </Dialog>
