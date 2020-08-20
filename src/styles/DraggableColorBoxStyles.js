68acbe483473c76be70633658ad1076bea6cadf4
Yun Chung <yun.chung@mac.com>
2020-08-20 11:30:15 +0900

    The text colors in the draggable color boxes are now dynamic

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,4 +1,5 @@
 import sizes from "./sizes";
+import chroma from 'chroma-js';
 
 const styles = {
   root: {
@@ -32,7 +33,8 @@ const styles = {
     width: "100%",
     left: "0px",
     bottom: "0px",
-    color: "rgba(0,0,0, 0.6)",
+    // color: "rgba(0,0,0, 0.6)",
+    color: props => chroma(props.color).luminance() <= 0.08 ? "rgba(255,255,255, 0.7)" : "rgba(0,0,0, 0.7)",
     letterSpacing: "1px",
     textTransform: "uppercase",
     fontSize: "12px",
